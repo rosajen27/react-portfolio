583d3bfc5df347f35954eaca1063422ba2563959
Jennifer Rosa <rosajen27@gmail.com>
2020-09-10 18:23:17 -0400

    update styes.css, create header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Header from "./components/Header"
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
-  );
+  )
+
 }
 
 export default App;
