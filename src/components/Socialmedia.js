09c555ad6653f8b58d3d9405c534923f8f2759e6
Jennifer Rosa <rosajen27@gmail.com>
2020-09-10 22:02:03 -0400

    Update Socialmedia.js

--- src/components/Socialmedia.js
+++ src/components/Socialmedia.js
@@ -4,8 +4,8 @@ import React from "react";
 function Socialmedia() {
     return (
         <div>
-        <img className="social-media-icon" src="https://github.com/rosajen27/react-portfolio/blob/master/assets/linkedin-icon.png?raw=true" alt="Linkedin" />
-        <img className="social-media-icon" src="https://github.com/rosajen27/react-portfolio/blob/master/assets/github-icon.png?raw=true" alt="Github" />
+        <img className="social-media-icon" src="https://github.com/rosajen27/react-portfolio/blob/master/src/assets/github-icon.png?raw=true" alt="Github" />
+        <img className="social-media-icon" src="https://github.com/rosajen27/react-portfolio/blob/master/src/assets/linkedin-icon.png?raw=true" alt="Linkedin" />
         </div>
     );
 }
