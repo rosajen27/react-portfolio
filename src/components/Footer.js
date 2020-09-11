8ddc27eb99bbaac80607266cd7cbcdbef04dd2e7
Jennifer Rosa <rosajen27@gmail.com>
2020-09-10 21:06:17 -0400

    social media component and images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Socialmedia from "./Socialmedia";
 
 function Footer() {
 
     const currentYear = new Date().getFullYear();
 
     return <footer>
+    <Socialmedia />
     <p>
         Copyright © Jennifer Rosa {currentYear}
     </p>
---
+++ src/components/Socialmedia.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Socialmedia() {
+  return (
+<div>
+    <a href="https://github.com/rosajen27"><img src="assets/github-icon.png" alt="GitHub"
+    class="social-media-icons" /></a>
+
+<a href="https://www.linkedin.com/in/rosajen27/"><img src="assets/linkedin-icon.png" alt="Linkedin"
+    class="social-media-icons" /></a>
+</div>
+  );
+}
+
+export default Socialmedia;
