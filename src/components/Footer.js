f030bb95eaf4529a4fa42cfa7dace555328621c8
Jennifer Rosa <rosajen27@gmail.com>
2020-09-10 20:56:35 -0400

    create and import footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+  
+import React from "react";
+
+function Footer() {
+
+    const currentYear = new Date().getFullYear();
+
+    return <footer>
+    <p>
+        Copyright © Jennifer Rosa {currentYear}
+    </p>
+    </footer>
+}
+
+export default Footer;
