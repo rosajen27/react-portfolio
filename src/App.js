8be10cfba1b88742a15a1e7a8799b030b5dea717
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 12:41:27 -0400

    rearrange and label css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Header from "./components/Header"
 import Footer from "./components/Footer";
-import About from "./components/About";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <About />
+      <Home />
       <Footer />
     </div>
   )
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+    <div className="home">
+    <h1 className="hometitle">Jennifer Rosa</h1>
+    <h2>Front End Web Developer</h2>
+    </div>
+    );
+}
+export default Home;
