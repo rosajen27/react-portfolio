04d5101fa30ae8579f6f612285db72e6d390173b
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 13:49:53 -0400

    change home bg and npm install font awesome

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ function Home() {
     <div className="home">
     <h1 className="hometitle">Jennifer Rosa</h1>
     <h2 className="hometitledescription">Front End Web Developer</h2>
+    <a href="#about"><i class="fas fa-chevron-circle-down"></i></a>
     </div>
     );
 }
