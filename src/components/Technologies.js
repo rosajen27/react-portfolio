4fa814d804ea6d912772bfc325bb4c01b0f81f28
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 19:51:04 -0400

    technologies component

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -5,6 +5,12 @@ function Technologies() {
         <div className="darkbg">
             <div className="technologies">
                 <h2>Technologies</h2>
+                <div>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/html.svg" alt="html-img"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/css.svg" alt="css-img"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/javascript.svg" alt="javascript-img"></img>
+                
+                </div>
             </div>
         </div>
     );
