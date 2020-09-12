ff768ce7434ecb8848eb7cf123f0dd8f110f0127
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 19:28:54 -0400

    create contact form and create technologies icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,31 @@ function Contact() {
         <div className="darkbg">
             <div className="contact">
                 <h2>Contact</h2>
-                <p>Lorem ipsum....</p>
+                <p>Contact me via <a href="mailto:rosajen27@gmail.com">email</a> or fill out the form below:</p>
+                <br></br><br></br>
+
+
+                <form action="mailto:rosajen27@gmail.com" method="post" enctype="text/plain">
+                    <label for="Name">Name</label>
+                    <br></br>
+                    <input pattern="[A-Z,a-z, ]*" type="text" name="name" id="Name"></input>
+                    <br></br><br></br>
+
+                    <label for="Email">Email</label>
+                    <br></br>
+                    <input type="text" name="mail" id="Email"></input>
+                    <br></br><br></br>
+
+                    <label for="Message">Message</label>
+                    <br></br>
+                    <textarea rows="5" name="message"></textarea>
+                    <br></br><br></br>
+
+                    <p><button type="submit">Submit</button></p>
+                </form>
             </div>
         </div>
+
     );
 }
 
