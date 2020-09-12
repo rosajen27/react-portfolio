e06f51901b6d3716abe785431fc8806a07e9ea04
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 12:06:41 -0400

    add header links

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 function About() {
-    return <h1>Jennifer Rosa</h1>
+    return <h1>About Section</h1>
 }
 export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function Header() {
   return <header>
-    <h1>Jennifer Rosa</h1>
+    <nav>
+      <ul id="nav" className="nav">
+        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+        <li><a className="smoothscroll" href="#about">About</a></li>
+        <li><a className="smoothscroll" href="#resume">Resume</a></li>
+        <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
+        <li><a className="smoothscroll" href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+
   </header>
 }
 
