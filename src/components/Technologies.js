a6aa595d09f2a3919ff92e8abc6959bd2910ea87
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 20:25:54 -0400

    technologies icons

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -9,7 +9,7 @@ function Technologies() {
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/html.svg" alt="html"></img>
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/css.svg" alt="css"></img>
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/4fa814d804ea6d912772bfc325bb4c01b0f81f28/public/icons/javascript.svg" alt="javascript"></img>
-                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/4fa814d804ea6d912772bfc325bb4c01b0f81f28/public/icons/jquery.svg" alt="jquery"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/facff5ae1d5f7b82d412f5431cf43acf57740516/public/icons/jquery.svg" alt="jquery"></img>
                 </div>
                 <div>
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/nodejs.svg" alt="node-js"></img>
