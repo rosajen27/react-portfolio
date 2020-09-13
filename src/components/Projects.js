b6075ae130ca63d159f27782b51ee8e0fe4eecc0
Jennifer Rosa <rosajen27@gmail.com>
2020-09-13 14:51:32 -0400

    projects component styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,8 @@ function Projects() {
         <div className="lightbg">
             <div className="projects">
                 <h2>Projects</h2>
+                <p className="projectsdescription">Here's a selection of some recent work.</p>
+                <p>For more up to the minute work you may visit my <a href="https://github.com/rosajen27"><strong>GitHub</strong></a> or <a href="#"><strong>Portfolio</strong></a>.</p>
                 <Project />
             </div>
         </div>
