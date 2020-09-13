5568c2a582707a9a043b8b4837dd826cef5d1a2c
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 20:28:43 -0400

    technologies icon

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -14,7 +14,7 @@ function Technologies() {
                 <div>
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/nodejs.svg" alt="node-js"></img>
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/react.svg" alt="react"></img>
-                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/4fa814d804ea6d912772bfc325bb4c01b0f81f28/public/icons/javascript.svg" alt="javascript"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/a6aa595d09f2a3919ff92e8abc6959bd2910ea87/public/icons/mongodb.svg" alt="mongo-db"></img>
                 </div>
             </div>
         </div>
