0e6ca6f17b6c4ca1c6072ed998521d628c132ddd
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 20:44:57 -0400

    finish technologies component

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -12,9 +12,9 @@ function Technologies() {
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/facff5ae1d5f7b82d412f5431cf43acf57740516/public/icons/jquery.svg" alt="jquery"></img>
                 </div>
                 <div>
-                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/nodejs.svg" alt="node-js"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/nodejs.svg" alt="node-and-express"></img>
                     <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/react.svg" alt="react"></img>
-                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/a6aa595d09f2a3919ff92e8abc6959bd2910ea87/public/icons/mongodb.svg" alt="mongo-db"></img>
+                    <img className="technologies-icon" src="https://github.com/rosajen27/react-portfolio/blob/master/public/icons/mongodb.png?raw=true" alt="mongo-db"></img>
                 </div>
             </div>
         </div>
