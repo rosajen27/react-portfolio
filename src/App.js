da76b41766602c717cabbbc7355df8c71a31e133
Jennifer Rosa <rosajen27@gmail.com>
2020-09-13 15:16:29 -0400

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Header from "./components/Header"
 import Footer from "./components/Footer";
 import Home from "./components/Home";
