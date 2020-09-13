facff5ae1d5f7b82d412f5431cf43acf57740516
Jennifer Rosa <rosajen27@gmail.com>
2020-09-12 20:21:38 -0400

    adding images to technologies component

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -6,10 +6,15 @@ function Technologies() {
             <div className="technologies">
                 <h2>Technologies</h2>
                 <div>
-                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/html.svg" alt="html-img"></img>
-                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/css.svg" alt="css-img"></img>
-                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/javascript.svg" alt="javascript-img"></img>
-                
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/html.svg" alt="html"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/css.svg" alt="css"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/4fa814d804ea6d912772bfc325bb4c01b0f81f28/public/icons/javascript.svg" alt="javascript"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/4fa814d804ea6d912772bfc325bb4c01b0f81f28/public/icons/jquery.svg" alt="jquery"></img>
+                </div>
+                <div>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/nodejs.svg" alt="node-js"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/ff768ce7434ecb8848eb7cf123f0dd8f110f0127/public/icons/react.svg" alt="react"></img>
+                    <img className="technologies-icon" src="https://raw.githubusercontent.com/rosajen27/react-portfolio/4fa814d804ea6d912772bfc325bb4c01b0f81f28/public/icons/javascript.svg" alt="javascript"></img>
                 </div>
             </div>
         </div>
