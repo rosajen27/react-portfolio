bea57d52da3dfda4589a6788e328ec861d891f77
Jennifer Rosa <rosajen27@gmail.com>
2020-09-18 18:47:52 -0400

    link resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
         <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
         <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
         <li className="nav-item"><a className="nav-link" href="#portfolio">Projects</a></li>
-        <li className="nav-item"><a className="nav-link" href="#resume">Resume</a></li>
+        <li className="nav-item"><a className="nav-link" href="https://rosajen27.github.io/assets/WebDevResume.pdf">Resume</a></li>
         <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
       </ul>
       </Nav>
