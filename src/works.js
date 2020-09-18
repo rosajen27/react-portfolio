97470550705b110532fd0a4f3bf65e1918dab31f
Jennifer Rosa <rosajen27@gmail.com>
2020-09-18 18:42:11 -0400

    new page when clicking social media links, adjust img links in portfolio

--- src/works.js
+++ src/works.js
@@ -4,7 +4,7 @@ const works = [
     projecttitle: "iNews",
     projecttech: "",
     projectdescription: "Coming Soon: Web application to find news articles. When the user goes to the front page they are presented with top news articles and search fields to find articles they are looking for.",
-    projectimg: "",
+    projectimg: "https://raw.githubusercontent.com/rosajen27/react-portfolio/master/public/images/comingsoon.png",
     deployedlink: "",
     repolink: "https://github.com/tkim30201/Project-Three"
   },
@@ -13,7 +13,7 @@ const works = [
     projecttitle: "TripReel",
     projecttech: "",
     projectdescription: "A mobile movie app that features dynamically updated HTML, CSS, and JavaScript while integrating data received from multiple server-side API requests.",
-    projectimg: "",
+    projectimg: "https://github.com/rosajen27/react-portfolio/blob/master/public/images/tripreel.png?raw=true",
     deployedlink: "https://rosajen27.github.io/trip-reel/",
     repolink: "https://github.com/rosajen27/trip-reel"
   },
@@ -22,7 +22,7 @@ const works = [
     projecttitle: "Songster",
     projecttech: "",
     projectdescription: "Songster is a mobile custom playlist creator that enables users to create awesome playlists of their favorite songs.",
-    projectimg: "",
+    projectimg: "https://github.com/rosajen27/react-portfolio/blob/master/public/images/songster.png?raw=true",
     deployedlink: "https://songster-project.herokuapp.com/",
     repolink: "https://github.com/rosajen27/songster"
   }
