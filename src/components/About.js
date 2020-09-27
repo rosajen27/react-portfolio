d465efa8a95657f6a1623e2ab194c7a4cdbda77f
Jennifer Rosa <rosajen27@gmail.com>
2020-09-27 18:26:48 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
                     <p className="aboutdescription">
                         <h2>Hello, World!</h2>
                         <br />
-                        My name is Jennifer Rosa, but most of my friends call me Jen for short. I am currently a K-6 Educator, based in Connecticut, who is transitioning out of the classroom and into the wonderful world of web development. When I am not teaching my lovely students, I am passionately creating dynamic and beautiful web pages as an ambitious Front-End Developer.
+                        My name is Jennifer Rosa. I am currently a 5th grade classroom teacher, based in Connecticut, who is transitioning out of the classroom and into the world of web development. When I am not teaching my lovely students, I am passionately creating dynamic and beautiful web pages as an ambitious and aspiring Front-End Developer.
                         <br /><br />
                         One of my dreams is to one day combine my teaching background and experience with my coding skills in order to support the development of computational thinking in young people. When I'm not online, I enjoy spending time outdoors, playing video games, powerlifting, and watching an irresponsible amount of Netflix.
                     </p>
