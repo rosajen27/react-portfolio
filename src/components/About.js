ad1fd3a9d30438a2c3e42fb3fdad663c8bde2f96
Jennifer Rosa <rosajen27@gmail.com>
2020-10-12 14:55:44 -0400

    xs mobile responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -6,17 +6,17 @@ function About() {
     return (
         <div className="lightbg container-fluid" id="about">
             <Row>
-                <Col className="mx-auto col-12 col-md-12 col-lg-6">
-                    <h2>Hello, World!</h2>
-                </Col>
-            </Row>
-
-            <Row>
                 <Col className="container-fluid col-12 col-md-12 col-lg-6">
+                <br></br><br></br>
+                <h2>Hello, World!</h2>
+                <br></br>
                     <p className="aboutdescription">
-                        I'm <b>Jen</b>, an elementary school teacher, based in <b>Connecticut</b>, who is transitioning out of the classroom and into the world of Web Development.<br></br><br></br>
-                        When I am not teaching my lovely students, I am passionately creating dynamic and beautiful web pages and web apps.<br></br><br></br>
-                        Through self-study and enrolling in a coding Bootcamp at the <b>University of Connecticut</b>, I am building a solid foundation in both frontend and backend technologies and languages.<br></br><br></br>
+                        I'm <b>Jen</b>, an elementary school teacher, based in <b>Connecticut</b>, who is transitioning out of the classroom and into the world of Web Development.
+                        <br></br><br></br><br></br>
+                        When I am not teaching my lovely students, I am passionately creating dynamic and beautiful web pages and web apps.
+                        <br></br><br></br><br></br>
+                        Through self-study and enrolling in a coding Bootcamp at the <b>University of Connecticut</b>, I am building a solid foundation in both frontend and backend technologies and languages.
+                        <br></br><br></br><br></br>
                         When I'm not online, I enjoy spending time outdoors, playing video games, powerlifting, and watching an irresponsible amount of Netflix.
                     </p>
                 </Col>
