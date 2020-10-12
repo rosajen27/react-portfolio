375a308213a3d4bd47c9b1db57243eeb618991ee
Jennifer Rosa <rosajen27@gmail.com>
2020-10-11 20:34:28 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,19 @@ import { Row, Col } from "react-bootstrap";
 
 function About() {
     return (
-            <Row className="lightbg container-fluid" id="about">
-                <Col className="mx-auto">
-                    <p className="aboutdescription">
-                        <h2>Hello, World!</h2>
-                        <br />
-                        My name is Jennifer Rosa. I am currently a 5th grade classroom teacher, based in Connecticut, who is transitioning out of the classroom and into the world of web development. When I am not teaching my lovely students, I am passionately creating dynamic and beautiful web pages as an ambitious and aspiring Front-End Developer.
-                        <br /><br />
-                        One of my dreams is to one day combine my teaching background and experience with my coding skills in order to support the development of computational thinking in young people. When I'm not online, I enjoy spending time outdoors, playing video games, powerlifting, and watching an irresponsible amount of Netflix.
-                    </p>
+        <Row className="lightbg container-fluid" id="about">
+            <Col className="mx-auto">
+                <p className="aboutdescription">
+                    <h2>Hello, World!</h2>
+                    <br />
+                    <p>I'm Jen, an elementary school teacher, based in <strong>Connecticut</strong>, who is transitioning out of the
+          classroom and into the world of Web Development.</p>
+                    <p>When I am not teaching my lovely students, I am passionately creating dynamic and beautiful web pages and web
+          apps.</p>
+                    <p>Through self-study and enrolling in a coding Bootcamp at the <strong>University of Connecticut</strong>, I am
+          building a solid foundation in both frontend and backend technologies and languages.</p>
+                    <p>When I'm not online, I enjoy spending time outdoors, playing video games, powerlifting, and watching an
+          irresponsible amount of Netflix.</p>
                 </Col>
                 <Col className="mx-auto container-fluid">
                     <img className="profile-img" src="https://github.com/rosajen27/react-portfolio/blob/master/public/images/mountain-profile3.png?raw=true" alt="profile-img" />
