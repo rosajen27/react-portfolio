1954b8492ceeb5967f3ed0091ad73fb6fbfc750c
Jennifer Rosa <rosajen27@gmail.com>
2020-10-12 14:01:26 -0400

    update img links

--- src/works.js
+++ src/works.js
@@ -4,7 +4,7 @@ const works = [
     projecttitle: "Daily News",
     projecttech: "",
     projectdescription: "News Web application, created with MERN, that allows the user to search, read, and save news articles.",
-    projectimg: "https://github.com/rosajen27/rosajen27.github.io/blob/master/public/images/news.png?raw=true",
+    projectimg: "https://rosajen27.github.io/public/images/news.png",
     deployedlink: "#projects",
     repolink: "https://github.com/rosajen27/news-app/"
   },
@@ -13,7 +13,7 @@ const works = [
     projecttitle: "TripReel",
     projecttech: "",
     projectdescription: "A mobile movie app that features dynamically updated HTML, CSS, and JavaScript while integrating data received from multiple server-side API requests.",
-    projectimg: "https://github.com/rosajen27/rosajen27.github.io/blob/master/public/images/tripreels.png?raw=true",
+    projectimg: "https://rosajen27.github.io/public/images/tripreels.png",
     deployedlink: "https://rosajen27.github.io/trip-reel/",
     repolink: "https://github.com/rosajen27/trip-reel"
   },
@@ -22,7 +22,7 @@ const works = [
     projecttitle: "Songster",
     projecttech: "",
     projectdescription: "Songster is a mobile custom playlist creator that enables users to create awesome playlists of their favorite songs.",
-    projectimg: "https://github.com/rosajen27/react-portfolio/blob/master/public/images/songster.png?raw=true",
+    projectimg: "https://rosajen27.github.io/public/images/songster.png",
     deployedlink: "https://songster-project.herokuapp.com/",
     repolink: "https://github.com/rosajen27/songster"
   },
@@ -31,7 +31,7 @@ const works = [
     projecttitle: "Weather Dashboard",
     projecttech: "",
     projectdescription: "A weather dashboard that will run in the browser and feature dynamically updated HTML and CSS. OpenWeather API was used to retrieve weather data for cities and incorporated localStorage to store any persistent data.",
-    projectimg: "https://github.com/rosajen27/react-portfolio/blob/master/public/images/weather.png?raw=true",
+    projectimg: "https://rosajen27.github.io/public/images/weather.png",
     deployedlink: "https://rosajen27.github.io/weather-dashboard/",
     repolink: "https://github.com/rosajen27/weather-dashboard"
   },
@@ -40,7 +40,7 @@ const works = [
     projecttitle: "Work Day Scheduler",
     projecttech: "",
     projectdescription: "A simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery and Moment.js.",
-    projectimg: "https://github.com/rosajen27/react-portfolio/blob/master/public/images/scheduler.png?raw=true",
+    projectimg: "https://rosajen27.github.io/public/images/scheduler.png",
     deployedlink: "https://rosajen27.github.io/work-day-scheduler/",
     repolink: "https://github.com/rosajen27/work-day-scheduler"
   },
@@ -49,7 +49,7 @@ const works = [
     projecttitle: "Eat Da Burger",
     projecttech: "",
     projectdescription: "Eat-Da-Burger! is a full-stack restaurant app that lets users input the names of burgers they'd like to eat.",
-    projectimg: "https://github.com/rosajen27/react-portfolio/blob/master/public/images/burger.png?raw=true",
+    projectimg: "https://rosajen27.github.io/public/images/burger.png",
     deployedlink: "https://rosa-burger.herokuapp.com/",
     repolink: "https://github.com/rosajen27/burger"
   }
