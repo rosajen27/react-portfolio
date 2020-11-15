e5b785eb9452105643dbfe295902d132be6fa852
Jennifer Rosa <rosajen27@gmail.com>
2020-11-15 16:23:04 -0500

    update projects

--- src/works.js
+++ src/works.js
@@ -1,39 +1,39 @@
 const works = [
   {
     id: 1,
-    projecttitle: "Daily News",
+    projecttitle: "Employee Directory",
     projecttech: "",
-    projectdescription: "News Web application, created with MERN, that allows the user to search, read, and save news articles.",
-    projectimg: "https://rosajen27.github.io/public/images/news.png",
-    deployedlink: "#projects",
-    repolink: "https://github.com/rosajen27/news-app/"
+    projectdescription: "An employee directory that enables users to search and view non-sensitive data about other employees.",
+    projectimg: "https://rosajen27.github.io/public/images/directory.png",
+    deployedlink: "https://rosa-employee-directory.herokuapp.com/",
+    repolink: "https://github.com/rosajen27/employee-directory"
   },
   {
     id: 2,
+    projecttitle: "Weather Dashboard",
+    projecttech: "",
+    projectdescription: "A weather dashboard that utilizes OpenWeather API to retrieve weather data for cities and incorporates localStorage to store data.",
+    projectimg: "https://rosajen27.github.io/public/images/weather.png",
+    deployedlink: "https://rosajen27.github.io/weather-dashboard/",
+    repolink: "https://github.com/rosajen27/weather-dashboard"
+  },
+  {
+    id: 3,
     projecttitle: "TripReel",
     projecttech: "",
-    projectdescription: "A mobile movie app that features dynamically updated HTML, CSS, and JavaScript while integrating data received from multiple server-side API requests.",
+    projectdescription: "A mobile movie app that integrates data received from multiple server-side API requests. This app was built from scratch collaboratively with a team of six coders, which allowed us to learn agile development methodologies and implement feature and bug fixes using the git branch workflow and pull requests.",
     projectimg: "https://rosajen27.github.io/public/images/tripreels.png",
     deployedlink: "https://rosajen27.github.io/trip-reel/",
     repolink: "https://github.com/rosajen27/trip-reel"
   },
   {
-    id: 3,
-    projecttitle: "Songster",
-    projecttech: "",
-    projectdescription: "Songster is a mobile custom playlist creator that enables users to create awesome playlists of their favorite songs.",
-    projectimg: "https://rosajen27.github.io/public/images/songster.png",
-    deployedlink: "https://songster-project.herokuapp.com/",
-    repolink: "https://github.com/rosajen27/songster"
-  },
-  {
     id: 4,
-    projecttitle: "Weather Dashboard",
+    projecttitle: "Eat Da Burger",
     projecttech: "",
-    projectdescription: "A weather dashboard that will run in the browser and feature dynamically updated HTML and CSS. OpenWeather API was used to retrieve weather data for cities and incorporated localStorage to store any persistent data.",
-    projectimg: "https://rosajen27.github.io/public/images/weather.png",
-    deployedlink: "https://rosajen27.github.io/weather-dashboard/",
-    repolink: "https://github.com/rosajen27/weather-dashboard"
+    projectdescription: "Eat-Da-Burger! is a full-stack restaurant app that lets users input the names of burgers they'd like to eat.",
+    projectimg: "https://rosajen27.github.io/public/images/burger.png",
+    deployedlink: "https://rosa-burger.herokuapp.com/",
+    repolink: "https://github.com/rosajen27/burger"
   },
   {
     id: 5,
@@ -46,19 +46,46 @@ const works = [
   },
   {
     id: 6,
-    projecttitle: "Eat Da Burger",
+    projecttitle: "Simon",
     projecttech: "",
-    projectdescription: "Eat-Da-Burger! is a full-stack restaurant app that lets users input the names of burgers they'd like to eat.",
-    projectimg: "https://rosajen27.github.io/public/images/burger.png",
-    deployedlink: "https://rosa-burger.herokuapp.com/",
-    repolink: "https://github.com/rosajen27/burger"
+    projectdescription: "The Simon game is the exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order. ",
+    projectimg: "https://rosajen27.github.io/public/images/simon.png",
+    deployedlink: "https://rosajen27.github.io/simon/",
+    repolink: "https://github.com/rosajen27/simon"
+  },
+  {
+    id: 7,
+    projecttitle: "FAQ",
+    projecttech: "",
+    projectdescription: "A FAQ card component created with HTML, CSS, and JavaScript.",
+    projectimg: "https://rosajen27.github.io/public/images/faq.png",
+    deployedlink: "https://rosajen27.github.io/faq-card/",
+    repolink: "https://github.com/rosajen27/faq-card"
+  },
+  {
+    id: 8,
+    projecttitle: "Profile Card",
+    projecttech: "",
+    projectdescription: "A Profile Card component created with HTML and CSS.",
+    projectimg: "https://rosajen27.github.io/public/images/profile.png",
+    deployedlink: "https://rosajen27.github.io/profile-card/",
+    repolink: "https://github.com/rosajen27/profile-card"
+  },
+  {
+    id: 9,
+    projecttitle: "To Do List",
+    projecttech: "",
+    projectdescription: "A simple to do list web app that allows user to add items on their to do list and cross them off when completed.",
+    projectimg: "https://rosajen27.github.io/public/images/todolist.png",
+    deployedlink: "https://rosa-todo.herokuapp.com/",
+    repolink: "https://github.com/rosajen27/todo"
   }
 ];
 
 export default works;
 
 /*
-
+{
     id: ,
     projecttitle: "",
     projecttech: "",
