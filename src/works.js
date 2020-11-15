9a6b6b4abec82c8bee5187f8a8174ea772d84262
Jennifer Rosa <rosajen27@gmail.com>
2020-11-15 16:30:19 -0500

    Update works.js

--- src/works.js
+++ src/works.js
@@ -1,60 +1,6 @@
 const works = [
   {
     id: 1,
-    projecttitle: "Employee Directory",
-    projecttech: "",
-    projectdescription: "An employee directory that enables users to search and view non-sensitive data about other employees.",
-    projectimg: "https://rosajen27.github.io/public/images/directory.png",
-    deployedlink: "https://rosa-employee-directory.herokuapp.com/",
-    repolink: "https://github.com/rosajen27/employee-directory"
-  },
-  {
-    id: 2,
-    projecttitle: "Weather Dashboard",
-    projecttech: "",
-    projectdescription: "A weather dashboard that utilizes OpenWeather API to retrieve weather data for cities and incorporates localStorage to store data.",
-    projectimg: "https://rosajen27.github.io/public/images/weather.png",
-    deployedlink: "https://rosajen27.github.io/weather-dashboard/",
-    repolink: "https://github.com/rosajen27/weather-dashboard"
-  },
-  {
-    id: 3,
-    projecttitle: "TripReel",
-    projecttech: "",
-    projectdescription: "A mobile movie app that integrates data received from multiple server-side API requests. This app was built from scratch collaboratively with a team of six coders, which allowed us to learn agile development methodologies and implement feature and bug fixes using the git branch workflow and pull requests.",
-    projectimg: "https://rosajen27.github.io/public/images/tripreels.png",
-    deployedlink: "https://rosajen27.github.io/trip-reel/",
-    repolink: "https://github.com/rosajen27/trip-reel"
-  },
-  {
-    id: 4,
-    projecttitle: "Eat Da Burger",
-    projecttech: "",
-    projectdescription: "Eat-Da-Burger! is a full-stack restaurant app that lets users input the names of burgers they'd like to eat.",
-    projectimg: "https://rosajen27.github.io/public/images/burger.png",
-    deployedlink: "https://rosa-burger.herokuapp.com/",
-    repolink: "https://github.com/rosajen27/burger"
-  },
-  {
-    id: 5,
-    projecttitle: "Work Day Scheduler",
-    projecttech: "",
-    projectdescription: "A simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery and Moment.js.",
-    projectimg: "https://rosajen27.github.io/public/images/scheduler.png",
-    deployedlink: "https://rosajen27.github.io/work-day-scheduler/",
-    repolink: "https://github.com/rosajen27/work-day-scheduler"
-  },
-  {
-    id: 6,
-    projecttitle: "Simon",
-    projecttech: "",
-    projectdescription: "The Simon game is the exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order. ",
-    projectimg: "https://rosajen27.github.io/public/images/simon.png",
-    deployedlink: "https://rosajen27.github.io/simon/",
-    repolink: "https://github.com/rosajen27/simon"
-  },
-  {
-    id: 7,
     projecttitle: "FAQ",
     projecttech: "",
     projectdescription: "A FAQ card component created with HTML, CSS, and JavaScript.",
@@ -63,22 +9,76 @@ const works = [
     repolink: "https://github.com/rosajen27/faq-card"
   },
   {
-    id: 8,
+    id: 2,
     projecttitle: "Profile Card",
     projecttech: "",
-    projectdescription: "A Profile Card component created with HTML and CSS.",
+    projectdescription: "A Profile Card component created with pure HTML and CSS.",
     projectimg: "https://rosajen27.github.io/public/images/profile.png",
     deployedlink: "https://rosajen27.github.io/profile-card/",
     repolink: "https://github.com/rosajen27/profile-card"
   },
   {
-    id: 9,
+    id: 3,
     projecttitle: "To Do List",
     projecttech: "",
     projectdescription: "A simple to do list web app that allows user to add items on their to do list and cross them off when completed.",
     projectimg: "https://rosajen27.github.io/public/images/todolist.png",
     deployedlink: "https://rosa-todo.herokuapp.com/",
     repolink: "https://github.com/rosajen27/todo"
+  },
+  {
+    id: 4,
+    projecttitle: "Work Day Scheduler",
+    projecttech: "",
+    projectdescription: "A simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery and Moment.js.",
+    projectimg: "https://rosajen27.github.io/public/images/scheduler.png",
+    deployedlink: "https://rosajen27.github.io/work-day-scheduler/",
+    repolink: "https://github.com/rosajen27/work-day-scheduler"
+  },
+  {
+    id: 5,
+    projecttitle: "Employee Directory",
+    projecttech: "",
+    projectdescription: "An employee directory that enables users to search and view non-sensitive data about other employees.",
+    projectimg: "https://rosajen27.github.io/public/images/directory.png",
+    deployedlink: "https://rosa-employee-directory.herokuapp.com/",
+    repolink: "https://github.com/rosajen27/employee-directory"
+  },
+  {
+    id: 6,
+    projecttitle: "Weather Dashboard",
+    projecttech: "",
+    projectdescription: "A weather dashboard that utilizes OpenWeather API to retrieve weather data for cities and incorporates localStorage to store data.",
+    projectimg: "https://rosajen27.github.io/public/images/weather.png",
+    deployedlink: "https://rosajen27.github.io/weather-dashboard/",
+    repolink: "https://github.com/rosajen27/weather-dashboard"
+  },
+  {
+    id: 7,
+    projecttitle: "Simon",
+    projecttech: "",
+    projectdescription: "The Simon game is the exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order. ",
+    projectimg: "https://rosajen27.github.io/public/images/simon.png",
+    deployedlink: "https://rosajen27.github.io/simon/",
+    repolink: "https://github.com/rosajen27/simon"
+  },
+  {
+    id: 8,
+    projecttitle: "Eat Da Burger",
+    projecttech: "",
+    projectdescription: "Eat-Da-Burger! is a full-stack restaurant app that lets users input the names of burgers they'd like to eat.",
+    projectimg: "https://rosajen27.github.io/public/images/burger.png",
+    deployedlink: "https://rosa-burger.herokuapp.com/",
+    repolink: "https://github.com/rosajen27/burger"
+  },
+  {
+    id: 9,
+    projecttitle: "TripReel",
+    projecttech: "",
+    projectdescription: "A mobile movie app that integrates data received from multiple server-side API requests. This app was built from scratch collaboratively with a team of six coders, which allowed us to learn agile development methodologies and implement feature and bug fixes using the git branch workflow and pull requests.",
+    projectimg: "https://rosajen27.github.io/public/images/tripreels.png",
+    deployedlink: "https://rosajen27.github.io/trip-reel/",
+    repolink: "https://github.com/rosajen27/trip-reel"
   }
 ];
 
