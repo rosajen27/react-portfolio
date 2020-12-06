7fdc6a396d483408b6eeb46343a23a27273cbd6c
Jennifer Rosa <rosajen27@gmail.com>
2020-12-06 18:30:16 -0500

    update styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ function Home() {
                 <Col>
                     <h1 className="home-title animation">Jennifer Rosa</h1>
                     <h2 className="animation">Full-Stack Web Developer</h2>
-                    <br></br><br></br><br></br><br></br><br></br><br></br>
+                    <br></br><br></br>
                     <p className="down-arrow animation"><a href="#about"><FontAwesomeIcon className="chevron-circle-down" icon="chevron-circle-down" size="2x" /></a></p>
                 </Col>
             </Row>
