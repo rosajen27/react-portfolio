5b3638fc1115b5feedd1a827a549824d5829b1f7
Jennifer Rosa <rosajen27@gmail.com>
2020-12-06 18:17:28 -0500

    Update works.js

--- src/works.js
+++ src/works.js
@@ -2,8 +2,8 @@ const works = [
   {
     id: 1,
     projecttitle: "FAQ",
-    projecttech: "",
-    projectdescription: "A FAQ card component created with HTML, CSS, and JavaScript.",
+    projecttech: "HTML, CSS, JavaScript",
+    projectdescription: "An interactive Frequently Ased Question card component.",
     projectimg: "https://rosajen27.github.io/public/images/faq.png",
     deployedlink: "https://rosajen27.github.io/faq-card/",
     repolink: "https://github.com/rosajen27/faq-card"
@@ -11,7 +11,7 @@ const works = [
   {
     id: 2,
     projecttitle: "Profile Card",
-    projecttech: "",
+    projecttech: "HTML, CSS",
     projectdescription: "A Profile Card component created with pure HTML and CSS.",
     projectimg: "https://rosajen27.github.io/public/images/profile.png",
     deployedlink: "https://rosajen27.github.io/profile-card/",
@@ -20,16 +20,16 @@ const works = [
   {
     id: 3,
     projecttitle: "To Do List",
-    projecttech: "",
+    projecttech: "HTML, CSS, JavaScript, Node.js + Express, EJS, MongoDB Atlas, Heroku",
     projectdescription: "A simple to do list web app that allows user to add items on their to do list and cross them off when completed.",
     projectimg: "https://rosajen27.github.io/public/images/todolist.png",
     deployedlink: "https://rosa-todo.herokuapp.com/",
-    repolink: "https://github.com/rosajen27/todo"
+    repolink: "https://github.com/rosajen27/todolist"
   },
   {
     id: 4,
     projecttitle: "Weather Dashboard",
-    projecttech: "",
+    projecttech: "HTML, CSS, JavaScript, jQuery, OpenWeather API",
     projectdescription: "A weather dashboard that utilizes OpenWeather API to retrieve weather data for cities and incorporates localStorage to store data.",
     projectimg: "https://rosajen27.github.io/public/images/weather.png",
     deployedlink: "https://rosajen27.github.io/weather-dashboard/",
@@ -38,7 +38,7 @@ const works = [
   {
     id: 5,
     projecttitle: "Work Day Scheduler",
-    projecttech: "",
+    projecttech: "HTML, CSS, JavaScript, jQuery, Moment.js",
     projectdescription: "A simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery and Moment.js.",
     projectimg: "https://rosajen27.github.io/public/images/scheduler.png",
     deployedlink: "https://rosajen27.github.io/work-day-scheduler/",
@@ -47,7 +47,7 @@ const works = [
   {
     id: 6,
     projecttitle: "Employee Directory",
-    projecttech: "",
+    projecttech: "HTML, CSS, JavaScript, JSX, Heroku, React",
     projectdescription: "An employee directory that enables users to search and view non-sensitive data about other employees.",
     projectimg: "https://rosajen27.github.io/public/images/directory.png",
     deployedlink: "https://rosa-employee-directory.herokuapp.com/",
@@ -56,7 +56,7 @@ const works = [
   {
     id: 7,
     projecttitle: "Eat Da Burger",
-    projecttech: "",
+    projecttech: "HTML, CSS, Node.js + Express, Handlebars, Heroku, MySQL",
     projectdescription: "Eat-Da-Burger! is a full-stack restaurant app that lets users input the names of burgers they'd like to eat.",
     projectimg: "https://rosajen27.github.io/public/images/burger.png",
     deployedlink: "https://rosa-burger.herokuapp.com/",
@@ -65,7 +65,7 @@ const works = [
   {
     id: 7,
     projecttitle: "Simon",
-    projecttech: "",
+    projecttech: "HTML, CSS, JavaScript",
     projectdescription: "The Simon game is the exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order. ",
     projectimg: "https://rosajen27.github.io/public/images/simon.png",
     deployedlink: "https://rosajen27.github.io/simon/",
@@ -74,7 +74,7 @@ const works = [
   {
     id: 9,
     projecttitle: "Secrets",
-    projecttech: "",
+    projecttech: "HTML, CSS, JavaScript, Node.js + Express, EJS, MongoDB Atlas, Heroku, Passport.js",
     projectdescription: "Secrets is a minimalist version of the Whisper app. Users can sign up and submit their secrets for all the world to see, but they don't have to tell the world who they are. We keep their secrets secret, by using advanced authentication methods. Shhh... it's a secret!",
     projectimg: "https://rosajen27.github.io/public/images/secrets.png",
     deployedlink: "https://rosa-secrets.herokuapp.com",
