56fd4758482506f7f0fd97585e66d3891731365b
Jennifer Rosa <rosajen27@gmail.com>
2020-12-06 09:32:02 -0500

    update navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,20 @@ import React from "react";
 function Header() {
   return (
     <div id="navigation">
-    <ul className="nav justify-content-end">
-      <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
-      <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-      <li className="nav-item"><a className="nav-link" href="#portfolio">Projects</a></li>
-      <li className="nav-item"><a className="nav-link" href="https://rosajen27.github.io/public/JENROSA.pdf" target="_blank">Resume</a></li>
-      <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
-    </ul>
+<ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" href="#home">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#about">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#portfolio">Projects</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#contact">Contact</a>
+  </li>
+</ul>
     </div>
   );
 }
