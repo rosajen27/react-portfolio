b7e044df1ef6920de67ff282765d1238530908fb
Jennifer Rosa <rosajen27@gmail.com>
2020-12-12 22:57:09 -0500

    projects card updates

--- src/works.js
+++ src/works.js
@@ -1,33 +1,42 @@
 const works = [
   {
     id: 1,
-    projecttitle: "FAQ",
-    projecttech: "HTML, CSS, JavaScript",
-    projectdescription: "An interactive Frequently Asked Question card component.",
-    projectimg: "https://rosajen27.github.io/public/images/faq.png",
-    deployedlink: "https://rosajen27.github.io/faq-card/",
-    repolink: "https://github.com/rosajen27/faq-card"
+    projecttitle: "Secrets",
+    projecttech: "HTML, CSS, JavaScript, Node.js + Express, EJS, MongoDB Atlas, Heroku, Passport.js, Google OAuth",
+    projectdescription: "Secrets is a minimalist version of the Whisper app. Users can sign up and submit their secrets for all the world to see, but they don't have to tell the world who they are.",
+    projectimg: "https://rosajen27.github.io/public/images/secrets.png",
+    deployedlink: "https://rosa-secrets.herokuapp.com",
+    repolink: "https://github.com/rosajen27/secrets"
   },
   {
     id: 2,
-    projecttitle: "Profile Card",
-    projecttech: "HTML, CSS",
-    projectdescription: "A Profile Card component created with pure HTML and CSS.",
-    projectimg: "https://rosajen27.github.io/public/images/profile.png",
-    deployedlink: "https://rosajen27.github.io/profile-card/",
-    repolink: "https://github.com/rosajen27/profile-card"
-  },
-  {
-    id: 3,
     projecttitle: "To Do List",
     projecttech: "HTML, CSS, JavaScript, Node.js + Express, EJS, MongoDB Atlas, Heroku",
-    projectdescription: "A simple to do list web app that allows user to add items on their to do list and cross them off when completed.",
+    projectdescription: "A simple to do list web app that allows the user to add items on their list and cross them off when completed.",
     projectimg: "https://rosajen27.github.io/public/images/todolist.png",
     deployedlink: "https://rosa-todo.herokuapp.com/",
     repolink: "https://github.com/rosajen27/todolist"
   },
   {
+    id: 3,
+    projecttitle: "Profile Card",
+    projecttech: "HTML, CSS",
+    projectdescription: "A simple profile card component.",
+    projectimg: "https://rosajen27.github.io/public/images/profile.png",
+    deployedlink: "https://rosajen27.github.io/profile-card/",
+    repolink: "https://github.com/rosajen27/profile-card"
+  },
+  {
     id: 4,
+    projecttitle: "FAQ",
+    projecttech: "HTML, CSS, JavaScript",
+    projectdescription: "An interactive frequently asked questions (FAQ) card component.",
+    projectimg: "https://rosajen27.github.io/public/images/faq.png",
+    deployedlink: "https://rosajen27.github.io/faq-card/",
+    repolink: "https://github.com/rosajen27/faq-card"
+  },
+  {
+    id: 5,
     projecttitle: "Weather Dashboard",
     projecttech: "HTML, CSS, JavaScript, jQuery, OpenWeather API",
     projectdescription: "A weather dashboard that utilizes OpenWeather API to retrieve weather data for cities and incorporates localStorage to store data.",
@@ -36,16 +45,16 @@ const works = [
     repolink: "https://github.com/rosajen27/weather-dashboard"
   },
   {
-    id: 5,
+    id: 6,
     projecttitle: "Work Day Scheduler",
     projecttech: "HTML, CSS, JavaScript, jQuery, Moment.js",
-    projectdescription: "A simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery and Moment.js.",
+    projectdescription: "A simple calendar application that allows the user to save events for each hour of the work day.",
     projectimg: "https://rosajen27.github.io/public/images/scheduler.png",
     deployedlink: "https://rosajen27.github.io/work-day-scheduler/",
     repolink: "https://github.com/rosajen27/work-day-scheduler"
   },
   {
-    id: 6,
+    id: 7,
     projecttitle: "Employee Directory",
     projecttech: "HTML, CSS, JavaScript, JSX, Heroku, React",
     projectdescription: "An employee directory that enables users to search and view non-sensitive data about other employees.",
@@ -54,7 +63,7 @@ const works = [
     repolink: "https://github.com/rosajen27/employee-directory"
   },
   {
-    id: 7,
+    id: 8,
     projecttitle: "Eat Da Burger",
     projecttech: "HTML, CSS, Node.js + Express, Handlebars, Heroku, MySQL",
     projectdescription: "Eat-Da-Burger! is a full-stack restaurant app that lets users input the names of burgers they'd like to eat.",
@@ -63,23 +72,14 @@ const works = [
     repolink: "https://github.com/rosajen27/burger"
   },
   {
-    id: 7,
+    id: 9,
     projecttitle: "Simon",
     projecttech: "HTML, CSS, JavaScript",
-    projectdescription: "The Simon game is the exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order. ",
+    projectdescription: "The exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order. ",
     projectimg: "https://rosajen27.github.io/public/images/simon.png",
     deployedlink: "https://rosajen27.github.io/simon/",
     repolink: "https://github.com/rosajen27/simon"
   },
-  {
-    id: 9,
-    projecttitle: "Secrets",
-    projecttech: "HTML, CSS, JavaScript, Node.js + Express, EJS, MongoDB Atlas, Heroku, Passport.js",
-    projectdescription: "Secrets is a minimalist version of the Whisper app. Users can sign up and submit their secrets for all the world to see, but they don't have to tell the world who they are. We keep their secrets secret, by using advanced authentication methods. Shhh... it's a secret!",
-    projectimg: "https://rosajen27.github.io/public/images/secrets.png",
-    deployedlink: "https://rosa-secrets.herokuapp.com",
-    repolink: "https://github.com/rosajen27/secrets"
-  }
 ];
 
 export default works;
@@ -95,13 +95,11 @@ export default works;
     repolink: ""
   },
 
-  
-
     {
-    id: 9,
+    id: 10,
     projecttitle: "TripReel",
-    projecttech: "",
-    projectdescription: "A mobile movie app that integrates data received from multiple server-side API requests. This app was built from scratch collaboratively with a team of six coders, which allowed us to learn agile development methodologies and implement feature and bug fixes using the git branch workflow and pull requests.",
+    projecttech: "HTML, CSS, JavaScript, jQuery, OMDB API, Utelly API",
+    projectdescription: "A mobile movie app that allows the user to search for relevant movie information and integrates data received from multiple server-side API requests.",
     projectimg: "https://rosajen27.github.io/public/images/tripreels.png",
     deployedlink: "https://rosajen27.github.io/trip-reel/",
     repolink: "https://github.com/rosajen27/trip-reel"
